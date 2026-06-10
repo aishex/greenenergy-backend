cc21fb0cd75ae33cd01e0c811d3cd27d80e2625d
Zuza <hynekzuzanna@gmail.com>
2026-06-10 14:39:08 +0200

    Init spring boot backend

---
+++ src/test/java/com/greenenergy/greenenergy/GreenenergyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenenergy.greenenergy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenenergyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
